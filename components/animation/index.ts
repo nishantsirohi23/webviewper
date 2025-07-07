bfa474cfe7698610a355c7e39dad4e7b7fca87bf
Nishant Sirohi <sirohinishant2307@gmail.com>
2025-07-08 04:37:16 +0530

    error removed animation folder

--- components/animation/index.ts
+++ components/animation/index.ts
@@ -1,4 +1,2 @@
 export { default as TextHover } from './TextHover';
-export { default as TextMarquee } from './LogoMarquee';
 export { default as TextMask } from './TextMask';
-export { default as LinkHover } from './LinkHover';
